6a7518fc34225684bebadffd720af69337e11ee5
Jonathan Gan <jonathang132298@gmail.com>
2024-12-08 14:11:11 -0500

    kms

--- src/app/utils/db.ts
+++ src/app/utils/db.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env, .env.local, or .env.development');
-}
+// if (!MONGODB_URI) {
+//   throw new Error('Please define the MONGODB_URI environment variable inside .env, .env.local, or .env.development');
+// }
 
 const dbConnect = mongoose.connect(MONGODB_URI as string).then((mongoose) => {
   return mongoose;
